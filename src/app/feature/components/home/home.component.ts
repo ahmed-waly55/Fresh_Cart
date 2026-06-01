9944904332df5a076ae9c298bdecdc74321ff71c
ahmed-waly55 <ahmedwaly074@gmail.com>
2026-06-01 13:56:37 +0300

    test integrate api

--- src/app/feature/components/home/home.component.ts
+++ src/app/feature/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../../shared/services/Products/products.service';
+import { IProducts } from '../../../core/interface/iproducts.interface';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent  implements OnInit {
+  constructor(private _ProductsService:ProductsService) {}
+
+  products!:IProducts[]
+
+  ngOnInit(): void {
+    this._ProductsService.getAllProducts().subscribe({
+      next:(res)=>{
+      this.products = res.data;
+      },
+      error:(err)=>{
+        console.log(err);
+      },
+      complete:()=>{}
+    })
+  }
+
+}
